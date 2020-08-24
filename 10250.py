27daa025c6298e3bd97bdbefc2974968d175ff26
jhsul <jhsul516@gmail.com>
2020-08-25 00:34:30 +0900

    better way

--- 10250.py
+++ 10250.py
@@ -2,7 +2,5 @@ import sys
 T = int(input())
 for i in range(T):
     H,W,N = map(int,sys.stdin.readline().split())
-    if N%H == 0:
-        print("%d%02d" %(H,N//H))
-    else:
-        print("%d%02d" %(N%H,N//H+1))
+    N -= 1
+    print("%d%02d" %(N%H+1,N//H+1))
